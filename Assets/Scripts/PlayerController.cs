14c079b12bd63491bae5c6e88ed7025bc77921e2
Samuel Mauldin <sammauldin01@gmail.com>
2021-11-11 19:55:19 -0600

    Player Controller Started
    Added in all player movement code. Dodge mechanic has started as a method and only has debug code in it. Also created an empty object for the enemy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,58 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    public CharacterController controller;
+    [SerializeField] float _speed = 6f;
+    [SerializeField] float _turnSmoothTime = 0.1f;
+    float _turnSmoothVelocity;
+    [SerializeField] float gravity = -9.81f;
+    [SerializeField] float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+    Vector3 velocity;
 
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * _speed * Time.deltaTime);
+        }
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Dodge();
+        }
+    }
+
+    void Dodge()
+    {
+        Debug.Log("DODGED");
     }
 }
